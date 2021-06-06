5ebbe5752122d883101a879b1e7d12f84475af39
Ariel Camhi <aacamhi@uc.cl>
2021-06-06 03:51:13 -0400

    Search

--- vue/app2/src/components/Search.vue
+++ vue/app2/src/components/Search.vue
@@ -1,14 +1,19 @@
 <template>
-<div></div>
-  
+  <div style=" width: 100%; height: 150px; padding: 10px">
+    <search-box :teamNumber="1"/>
+    <search-box :teamNumber="2"/>
+  </div>
 </template>
 
 <script>
-export default {
+import SearchBox from "../components/SearchBox.vue";
 
-}
+export default {
+  components: {
+    SearchBox,
+  },
+};
 </script>
 
 <style>
-
 </style>
